a2836a706dd601ff2a1811e8475d35c7e1f2141f
wilomaku <wilomaku@gmail.com>
2024-02-13 20:24:57 +0100

    Updated Firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDV_4MUzqS7Ax9wrj1o55iEYJqfl_mFmhM",
-  authDomain: "dbluca-41333.firebaseapp.com",
-  databaseURL: "https://dbluca-41333-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dbluca-41333",
-  storageBucket: "dbluca-41333.appspot.com",
-  messagingSenderId: "523225576774",
-  appId: "1:523225576774:web:2c12a8822ff8ec25c32147"
+  apiKey: "AIzaSyCdNjTpI6ZZvZ4NinQdSLspEQlvsH8SKuQ",
+  authDomain: "lucafb-7ed31.firebaseapp.com",
+  projectId: "lucafb-7ed31",
+  storageBucket: "lucafb-7ed31.appspot.com",
+  messagingSenderId: "64969744279",
+  appId: "1:64969744279:web:9982534d274a500678aed2",
+  measurementId: "G-FHM620VMC9"
 };
 
 // Initialize Firebase
