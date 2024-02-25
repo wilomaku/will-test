ba098b5a138317d3d67230af4885a3c816bedc30
wilomaku <wilomaku@gmail.com>
2024-02-25 13:43:36 +0100

    Adjusted categories

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ function App() {
     setCategory('');
     setIsIncome(null);
   };
-  
+
   useEffect(() => {
     const db = getDatabase();
     const entriesData = ref(db, 'entries');
-    
+
     onValue(entriesData, (snapshot) => {
       const data = snapshot.val();
       // make an array out of the object to render it nicely
@@ -38,7 +38,7 @@ function App() {
   useEffect(() => {
     const db = getDatabase();
     const categoriesObject = ref(db, 'categories');
-    
+
     onValue(categoriesObject, (snapshot) => {
       const data = snapshot.val();
 
@@ -58,16 +58,16 @@ function App() {
       amount: amount,
       timestamp: new Date().getTime()
     };
-  
+
     // Get a key for a new Post.
     const newPostKey = push(child(ref(db), 'posts')).key;
-  
+
     console.log(newPostKey);
 
     // Write the new post's data simultaneously in the posts list and the user's post list.
     const updates = {};
     updates['/entries/' + newPostKey] = postData;
-    
+
     update(ref(db), updates);
 
     resetForm();
@@ -78,16 +78,16 @@ function App() {
       <div className="relative isolate px-6 py-8 lg:px-8">
         <div className="mx-auto max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">
-            Income + Expenses
+            Luca Registra
           </h1>
-          
+
           <form className="w-full max-w-lg" onSubmit={onSubmit}>
             <div className="mt-6">
-              <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+              <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
               <input value={amount} onChange={(e) => setAmount(e.target.value)} type="text" id="amount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
 
-            <label className="block mt-6 mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+            <label className="block mt-6 mb-2 text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
             <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
               {categories.map(({id, value}) => (
                 <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600" key={id}>
@@ -102,10 +102,10 @@ function App() {
             <ul className="grid w-full my-6 gap-6 md:grid-cols-2">
               <li>
                 <input checked={isIncome === true} onChange={() => setIsIncome(true)} type="radio" id="income" value="income" className="hidden peer" required/>
-                <label htmlFor="income" className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                <label htmlFor="income" className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                   <div className="block">
-                    <div className="w-full text-m font-semibold">Income</div>
-                    <div className="w-full">Money you earned</div>
+                    <div className="w-full text-m font-semibold">Entrada</div>
+                    <div className="w-full">Dinero ganado</div>
                   </div>
                 </label>
               </li>
@@ -113,15 +113,15 @@ function App() {
                 <input checked={isIncome === false} onChange={() => setIsIncome(false)} type="radio" id="expense" value="expense" className="hidden peer"/>
                 <label htmlFor="expense" className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                   <div className="block">
-                    <div className="w-full text-m font-semibold">Expense</div>
-                    <div className="w-full">Money you spent</div>
+                    <div className="w-full text-m font-semibold">Salida</div>
+                    <div className="w-full">Dinero gastado</div>
                   </div>
                 </label>
               </li>
             </ul>
 
-            <button type="button" onClick={() => resetForm()} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Cancel</button>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+            <button type="button" onClick={() => resetForm()} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Cancelar</button>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Enviar</button>
           </form>
 
           <Table entries={entries} categories={categories} />
@@ -137,10 +137,10 @@ const Table = ({entries, categories}) => (
     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">Amount</th>
-            <th scope="col" className="px-6 py-3">Category</th>
-            <th scope="col" className="px-6 py-3">Type</th>
-            <th scope="col" className="px-6 py-3">Date</th>
+            <th scope="col" className="px-6 py-3">Cantidad</th>
+            <th scope="col" className="px-6 py-3">Categoria</th>
+            <th scope="col" className="px-6 py-3">Tipo</th>
+            <th scope="col" className="px-6 py-3">Fecha</th>
           </tr>
       </thead>
 
@@ -154,8 +154,8 @@ const Table = ({entries, categories}) => (
               {categories.find(item => item.id === entry.category)?.value}
             </td>
             <td className="px-6 py-4">
-              {entry.isIncome && (<><IncomeIcon /><p className="text-green-500">Income</p></>)}
-              {!entry.isIncome && (<><ExpenseIcon /><p className="text-red-600">Expense</p></>)}
+              {entry.isIncome && (<><IncomeIcon /><p className="text-green-500">Entrada</p></>)}
+              {!entry.isIncome && (<><ExpenseIcon /><p className="text-red-600">Salida</p></>)}
             </td>
             <td className="px-6 py-4">
               {new Date(entry.timestamp).toLocaleDateString()}
